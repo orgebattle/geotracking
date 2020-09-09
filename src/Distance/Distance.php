d68629260e4b816acc0d66858d41a7c70017b620
Orgebattle <orgebattlesaga@gmail.com>
2020-09-09 15:23:55 +0900

    Init

---
+++ src/Distance/Distance.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace orgebattle\Distance;
+
+Class Distance
+{
+    /**
+     * @var int
+     */
+    protected int $txPower;
+
+    /**
+     * @var int
+     */
+    protected int $rssi;
+
+    /**
+     * depends on environmental factors
+     * 2 for free space, 2.7 to 3.5 for urban areas,
+     * 3.0 to 5.0 in suburban areas, 1.6 to 1.8 for indoors
+     * @var int
+     */
+    protected int $factor = 2;
+
+    /**
+     * Distance constructor.
+     * @param $txPower
+     * @param $rssi
+     * @param $factor
+     */
+    public function __construct($txPower, $rssi, $factor)
+    {
+        $this->txPower = $txPower;
+        $this->rssi = $rssi;
+        $this->factor = $factor;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTxPower(): int
+    {
+        return $this->txPower;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRssi(): int
+    {
+        return $this->rssi;
+    }
+
+    /**
+     * @return float
+     */
+    public function calculate(): float
+    {
+        if (!$this->txPower || !$this->rssi) {
+            return -1;
+        }
+
+        return pow(10, ($this->txPower - $this->rssi) / (10 * $this->factor));
+    }
+
+}
---
+++ src/Trilateration/Sphere.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of trilateration package
+ *
+ * Copyright (c) 2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/trilateration
+ *
+ */
+
+namespace orgebattle\Trilateration;
+
+class Sphere extends Point
+{
+    protected float $radius;
+
+    public function __construct($latitude, $longitude, $radius)
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        $this->radius = $radius;
+    }
+
+    public function radius()
+    {
+        return $this->radius;
+    }
+
+    public function enlarge($meters)
+    {
+        return new Sphere($this->latitude, $this->longitude, $this->radius + $meters);
+    }
+
+    public function __toString()
+    {
+        return "{$this->latitude},{$this->longitude},{$this->radius}";
+    }
+}
